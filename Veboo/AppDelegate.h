8b9bd8347676968f70048c1937200f688d7ee746
heqingbao <andro.he@gmail.com>
2014-07-13 13:45:53 +0800

    Initial Commit

---
+++ Veboo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Veboo
+//
+//  Created by HeQingbao on 14-7-13.
+//  Copyright (c) 2014年 HeQingbao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
